5b36ec2ffc65dc297985bf826241df0b92ae4f5f
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-09-30 14:32:01 +0700

    adding navbar and post page

---
+++ src/app/component/navbar.php
@@ -0,0 +1,17 @@
+<div class="topnav">
+    <div class="">
+        <div class="logo"></div>
+    </div>
+    <a class="text-1">Home</a>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <circle cx="11" cy="11" r="7" stroke="#33363F" stroke-width="2"/>
+        <path d="M20 20L17 17" stroke="#33363F" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+    <input class="search-bar" type="text" placeholder="Search..">
+    <div class=flex-5>
+        <img src="https://i.ibb.co/yh0nx29/niki.jpg" class="logo"></img>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="39" height="38" viewBox="0 0 39 38" fill="none">
+        <path d="M29.25 14.1818L19.5 23.6364L9.75 14.1818" stroke="#5D6386" stroke-width="2.36364"/>
+    </svg>
+</div>
