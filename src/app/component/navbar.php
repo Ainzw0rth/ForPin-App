8c547aab42f51d8dae626e5ec794ad88fb9f5693
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-09-30 15:33:19 +0700

    adjusting post page

--- src/app/component/navbar.php
+++ src/app/component/navbar.php
@@ -1,17 +1,21 @@
 <div class="topnav">
-    <div class="">
+    <div class="flex align-center">
         <div class="logo"></div>
+        <a class="text-1">Home</a>
     </div>
-    <a class="text-1">Home</a>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <circle cx="11" cy="11" r="7" stroke="#33363F" stroke-width="2"/>
-        <path d="M20 20L17 17" stroke="#33363F" stroke-width="2" stroke-linecap="round"/>
-    </svg>
+    <div class="flex align-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <circle cx="11" cy="11" r="7" stroke="#33363F" stroke-width="2"/>
+            <path d="M20 20L17 17" stroke="#33363F" stroke-width="2" stroke-linecap="round"/>
+        </svg>
     <input class="search-bar" type="text" placeholder="Search..">
-    <div class=flex-5>
-        <img src="https://i.ibb.co/yh0nx29/niki.jpg" class="logo"></img>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" width="39" height="38" viewBox="0 0 39 38" fill="none">
-        <path d="M29.25 14.1818L19.5 23.6364L9.75 14.1818" stroke="#5D6386" stroke-width="2.36364"/>
-    </svg>
+    <div class="flex align-center">
+        <div>
+            <img src="https://i.ibb.co/5rjj8n2/Cat-Medley-Cuteness-Galore-Funnies-Rescues-And-Appreciation.png" class="logo"></img>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="38" viewBox="0 0 39 38" fill="none">
+            <path d="M29.25 14.1818L19.5 23.6364L9.75 14.1818" stroke="#5D6386" stroke-width="2.36364"/>
+        </svg>
+    </div>
 </div>
