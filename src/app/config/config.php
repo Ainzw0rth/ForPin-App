bdfb72534fc787ec1a03d4d05d961efefd2b6a02
louis <louiscaesapdf@gmail.com>
2023-09-30 18:08:41 +0700

    add image viewer to homepage

---
+++ src/app/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+// Directory
+define('BASE_DIR', 'http://localhost:80/src/public');
+define('IMAGE_DIR', 'http://localhost:80/src/public/images/temp_logo.png');
+define('STYLE_DIR', 'http://localhost:80/src/public/styles');
