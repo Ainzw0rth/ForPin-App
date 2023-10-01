3d4d2902d4f2dd4e7cb77657cfc3c881cf6d70c4
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-10-01 21:09:36 +0700

    refactoring homepage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="public/css/globals.css">
     <link rel="stylesheet" type="text/css" href="public/css/header.css">
+    <link rel="stylesheet" type="text/css" href="public/css/homepage/homepage.css">
     <title>ForPin | Home</title>
 </head>
 <body>
     <?php include('app/views/component/navbar.php'); ?>
+    <div class="media_canvas" id="media_canvas">
+        </div>
+        
+    <script src="http://localhost:8080/public/js/homepage/homepage.js"></script>
+    <script src="http://localhost:8080/public/js/navbar/navbar.js"></script>
 </body>
 </html>
