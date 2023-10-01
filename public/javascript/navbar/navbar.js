24e41cf425d3c9612611a4f43700f374cfe30262
louis <louiscaesapdf@gmail.com>
2023-10-01 21:38:33 +0700

    add responsive navbar

--- public/javascript/navbar/navbar.js
+++ public/javascript/navbar/navbar.js
@@ -1,14 +1,16 @@
 function expandOptions() {
-    document.getElementById("hidden").style.display = "block";
+    document.getElementById("inputbox").style.display = "block";
+    document.getElementById("temp").style.marginTop = "60px";
 }
 
 function closeOptions() {
-    document.getElementById("hidden").style.display = "none";
+    document.getElementById("inputbox").style.display = "none";
+    document.getElementById("temp").style.marginTop = "3px";
 }
 
 // for expanding the navbar
 document.addEventListener("click", function(event) {
-    if (document.getElementById("hidden").style.display == "block") {
+    if (document.getElementById("inputbox").style.display == "block") {
         var validSearchBox = document.getElementById("searchbox2");
         var validInputBox = document.getElementById("inputbox");
         if (event.target != validInputBox && event.target != validSearchBox && !validInputBox.contains(event.target)) {
