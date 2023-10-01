28e8bff5b4ed25f9794f2db4b3951102eb5f698d
louis <louiscaesapdf@gmail.com>
2023-10-01 15:36:19 +0700

    add filter & sort, home button, and profile button to navbar

--- src/app/components/homepage/homepage.php
+++ src/app/components/homepage/homepage.php
@@ -9,16 +9,16 @@
     <link rel="stylesheet" type="text/css" href="http://localhost:8080/src/public/styles/header.css">
     <link rel="stylesheet" type="text/css" href="http://localhost:8080/src/public/styles/homepage/homepage.css">
     <link rel="stylesheet" type="text/css" href="http://localhost:8080/src/public/styles/globals.css">
-
     <title>Home Page</title>
 </head>
 
 <body>
     <?php include('../../component/navbar.php'); ?>
     <div class="media_canvas" id="media_canvas">
-    </div>
-
+        </div>
+        
     <script src="http://localhost:8080/src/public/javascript/homepage/homepage.js"></script>
+    <script src="http://localhost:8080/src/public/javascript/navbar/navbar.js"></script>
 </body>
 
 </html>
