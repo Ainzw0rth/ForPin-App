50d1d5ccbb565294bbcc56c08f28537f2d2df771
louis <louiscaesapdf@gmail.com>
2023-10-01 16:17:31 +0700

    rebase develop-homepage with develop

--- src/app/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Directory
-define('BASE_DIR', 'http://localhost:80/src/public');
-define('IMAGE_DIR', 'http://localhost:80/src/public/images/temp_logo.png');
-define('STYLE_DIR', 'http://localhost:80/src/public/styles');
