97697c32ef1f5e0ff4db46ea4f5182cb229f5bcf
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-10-02 23:08:41 +0700

    adding javascript for post page

--- app/controllers/Post.php
+++ app/controllers/Post.php
@@ -7,6 +7,8 @@ class Post extends Controller {
         $userId = $this->model('User_post_model')->getUserId($currentPost);
         $data['user'] = $this->model('User_model')->getUserDesc($userId['user_id']);
         $data['post'] = $this->model('Post_model')->getPostElements($currentPost);
+        $data['img'] = $this->model('Image_model')->getUserImagePath($currentPost);
+        $data['vid'] = $this->model('Video_model')->getUserVideoPath($currentPost);
         $this->view('post/index', $data);
     }
 }
--- app/views/post/index.php
+++ app/views/post/index.php
@@ -12,9 +12,11 @@
     <?php include('app/views/component/navbar.php'); ?>
     <div class="center-container">
         <div class=black-rectangle>
-            <img src="https://i.ibb.co/5rjj8n2/Cat-Medley-Cuteness-Galore-Funnies-Rescues-And-Appreciation.png" class="image-container img-black-rectangle"></img>
+            <!-- should use for each and carousel for multiple post -->
+                <img src="<?= $data['img'][0]['img_path'] ?>" class="image-container img-black-rectangle"></img>
+                <!-- load video too -->
             <div class="content-container">
-                <div class="flex padding-25 align-center justify-between gap-10">
+                <div class="flex padding-25 align-center gap-10">
                     <img src="<?= $data['user']['profile_path'] ?>" class=profile-picture></img>
                     <div>
                         <p class=text-1><?= $data['user']['username'] ?></p>
@@ -28,14 +30,15 @@
                     <p class=text-1><?= $data['post']['caption'] ?></p>
                     <p class=text-2><?= $data['post']['description'] ?></p>
                 </div>
-                <p class=text-2><?= $data['post']['likes'] ?></p>
                 <div class="svg-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37" fill="none">
-                        <path d="M6.86141 21.4418L17.58 31.5108L17.58 31.5108C17.9493 31.8576 18.1339 32.031 18.3515 32.0738C18.4495 32.093 18.5504 32.093 18.6484 32.0738C18.8661 32.031 19.0507 31.8576 19.4199 31.5108L30.1385 21.4418C33.1543 18.6088 33.5205 13.9468 30.9841 10.6776L30.5072 10.0629C27.4729 6.1521 21.3823 6.80798 19.2502 11.2752C18.9491 11.9062 18.0508 11.9062 17.7497 11.2752C15.6176 6.80798 9.52703 6.1521 6.49276 10.0629L6.01583 10.6776C3.47942 13.9468 3.84564 18.6088 6.86141 21.4418Z" stroke="white" stroke-width="1.54167"/>
+                    <p class=text-1-normal><?= $data['post']['likes'] ?></p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37" fill="none" id="likes" class="pointer">
+                        <path d="M6.86141 21.4418L17.58 31.5108L17.58 31.5108C17.9493 31.8576 18.1339 32.031 18.3515 32.0738C18.4495 32.093 18.5504 32.093 18.6484 32.0738C18.8661 32.031 19.0507 31.8576 19.4199 31.5108L30.1385 21.4418C33.1543 18.6088 33.5205 13.9468 30.9841 10.6776L30.5072 10.0629C27.4729 6.1521 21.3823 6.80798 19.2502 11.2752C18.9491 11.9062 18.0508 11.9062 17.7497 11.2752C15.6176 6.80798 9.52703 6.1521 6.49276 10.0629L6.01583 10.6776C3.47942 13.9468 3.84564 18.6088 6.86141 21.4418Z" stroke="white" stroke-width="1.54167"  id="heart"/>
                     </svg>
                 </div>
             </div>
         </div>
     </div>
+    <script src="public/javascript/post/post.js"></script>
 </body>
 </html>
