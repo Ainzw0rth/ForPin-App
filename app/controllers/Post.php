69e00cc461b9d21a39e7effe2e67eae8774c11ad
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-10-03 16:56:28 +0700

    ajax for post likes

--- app/controllers/Post.php
+++ app/controllers/Post.php
@@ -11,4 +11,22 @@ class Post extends Controller {
         $data['vid'] = $this->model('Video_model')->getUserVideoPath($currentPost);
         $this->view('post/index', $data);
     }    
+
+    public function addLikes() {
+        if (isset($_POST['action']) && isset($_POST['postId'])) {
+            $action = $_POST['action'];
+            $postId = $_POST['postId'];
+        
+            if ($action === "add") {
+                $this->model("Post_model")->addLikes($postId);
+            } else {
+                $this->model("Post_model")->substractLikes($postId);
+            }
+        
+            header('Content-Type: application/json'); 
+            echo json_encode($this->model("Post_model")->getLikes($postId));
+        } else {
+            echo "Invalid Request";
+        }
+    }
 }
--- public/javascript/post/post.js
+++ public/javascript/post/post.js
@@ -1,15 +1,39 @@
 const heartIcon = document.getElementById("likes");
 const heart = document.getElementById("heart");
 
-console.log(postId);
 
 heartIcon.addEventListener("click", function () {
-    heart.classList.toggle("blue-heart");
-    if (heart.classList.contains("blue-heart")) {
-        // addLikes($postId)
-        heart.setAttribute("stroke", "transparent");
-    } else {
-        heart.setAttribute("stroke", "white");
-        // substractLikes($postId)
+    const isLiked = heart.classList.contains("blue-heart");
+    const action = isLiked ? 'substract' : 'add';
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", `/post/addLikes`);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            if (action === "add") {
+                heart.classList.add("blue-heart");
+                heart.setAttribute("stroke", "transparent");
+                console.log(this.responseText);
+                const response = JSON.parse(this.responseText);
+                const likes = response.likes;
+                console.log(likes);
+                document.getElementById("likes_number").textContent = likes;
+            } else {
+                heart.setAttribute("stroke", "white");
+                heart.classList.remove("blue-heart");
+                console.log(this.responseText);
+                const response = JSON.parse(this.responseText);
+                const likes = response.likes;
+                console.log(likes);
+                document.getElementById("likes_number").textContent = likes;
+            }
+        }
     }
+    console.log(action)
+    console.log(postId);
+    const formData = new FormData();
+    formData.append("action", action);
+    formData.append("postId", postId);
+    xhr.send(formData);
 })
