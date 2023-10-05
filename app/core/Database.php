f20c2e36f5de0ef411d9ee1a71d2d4dc5a3c4cf6
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-10-05 12:03:11 +0700

    fix database and create page

--- app/core/Database.php
+++ app/core/Database.php
@@ -114,102 +114,102 @@ class Database {
         );
         $this->execute();
         
-        // Post
-        $this->query("INSERT INTO post (caption, descriptions, post_time, likes, genre) VALUES
-            ('Post 1 Caption', 'Description for Post 1', '2023-10-01', 10, 'happy, horror, meme'),
-            ('Post 2 Caption', 'Description for Post 2', '2023-10-02', 15, 'horror'),
-            ('Post 3 Caption', 'Description for Post 3', '2023-10-03', 5, 'happy'),
-            ('Post 4 Caption', 'Description for Post 4', '2023-10-04', 20, 'meme'),
-            ('Post 5 Caption', 'Description for Post 5', '2023-10-05', 8, 'horror'),
-            ('Post 6 Caption', 'Description for Post 6', '2023-10-06', 12, 'happy'),
-            ('Post 7 Caption', 'Description for Post 7', '2023-10-07', 4, 'meme'),
-            ('Post 8 Caption', 'Description for Post 8', '2023-10-08', 25, 'horror'),
-            ('Post 9 Caption', 'Description for Post 9', '2023-10-09', 14, 'happy'),
-            ('Post 10 Caption', 'Description for Post 10', '2023-10-10', 30, 'meme'),
-            ('Post 11 Caption', 'Description for Post 11', '2023-10-11', 7, 'horror'),
-            ('Post 12 Caption', 'Description for Post 12', '2023-10-12', 18, 'happy'),
-            ('Post 13 Caption', 'Description for Post 13', '2023-10-13', 9, 'meme'),
-            ('Post 14 Caption', 'Description for Post 14', '2023-10-14', 22, 'horror'),
-            ('Post 15 Caption', 'Description for Post 15', '2023-10-15', 6, 'happy'),
-            ('Post 16 Caption', 'Description for Post 16', '2023-10-16', 11, 'meme'),
-            ('Post 17 Caption', 'Description for Post 17', '2023-10-17', 13, 'horror'),
-            ('Post 18 Caption', 'Description for Post 18', '2023-10-18', 28, 'happy'),
-            ('Post 19 Caption', 'Description for Post 19', '2023-10-19', 7, 'meme'),
-            ('Post 20 Caption', 'Description for Post 20', '2023-10-20', 31, 'horror'),
-            ('Post 21 Caption', 'Description for Post 21', '2023-10-21', 10, 'happy'),
-            ('Post 22 Caption', 'Description for Post 22', '2023-10-22', 16, 'meme'),
-            ('Post 23 Caption', 'Description for Post 23', '2023-10-23', 15, 'horror'),
-            ('Post 24 Caption', 'Description for Post 24', '2023-10-24', 19, 'happy');"
-        );
-        $this->execute();
+        // // Post
+        // $this->query("INSERT INTO post (caption, descriptions, post_time, likes, genre) VALUES
+        //     ('Post 1 Caption', 'Description for Post 1', '2023-10-01', 10, 'happy, horror, meme'),
+        //     ('Post 2 Caption', 'Description for Post 2', '2023-10-02', 15, 'horror'),
+        //     ('Post 3 Caption', 'Description for Post 3', '2023-10-03', 5, 'happy'),
+        //     ('Post 4 Caption', 'Description for Post 4', '2023-10-04', 20, 'meme'),
+        //     ('Post 5 Caption', 'Description for Post 5', '2023-10-05', 8, 'horror'),
+        //     ('Post 6 Caption', 'Description for Post 6', '2023-10-06', 12, 'happy'),
+        //     ('Post 7 Caption', 'Description for Post 7', '2023-10-07', 4, 'meme'),
+        //     ('Post 8 Caption', 'Description for Post 8', '2023-10-08', 25, 'horror'),
+        //     ('Post 9 Caption', 'Description for Post 9', '2023-10-09', 14, 'happy'),
+        //     ('Post 10 Caption', 'Description for Post 10', '2023-10-10', 30, 'meme'),
+        //     ('Post 11 Caption', 'Description for Post 11', '2023-10-11', 7, 'horror'),
+        //     ('Post 12 Caption', 'Description for Post 12', '2023-10-12', 18, 'happy'),
+        //     ('Post 13 Caption', 'Description for Post 13', '2023-10-13', 9, 'meme'),
+        //     ('Post 14 Caption', 'Description for Post 14', '2023-10-14', 22, 'horror'),
+        //     ('Post 15 Caption', 'Description for Post 15', '2023-10-15', 6, 'happy'),
+        //     ('Post 16 Caption', 'Description for Post 16', '2023-10-16', 11, 'meme'),
+        //     ('Post 17 Caption', 'Description for Post 17', '2023-10-17', 13, 'horror'),
+        //     ('Post 18 Caption', 'Description for Post 18', '2023-10-18', 28, 'happy'),
+        //     ('Post 19 Caption', 'Description for Post 19', '2023-10-19', 7, 'meme'),
+        //     ('Post 20 Caption', 'Description for Post 20', '2023-10-20', 31, 'horror'),
+        //     ('Post 21 Caption', 'Description for Post 21', '2023-10-21', 10, 'happy'),
+        //     ('Post 22 Caption', 'Description for Post 22', '2023-10-22', 16, 'meme'),
+        //     ('Post 23 Caption', 'Description for Post 23', '2023-10-23', 15, 'horror'),
+        //     ('Post 24 Caption', 'Description for Post 24', '2023-10-24', 19, 'happy');"
+        // );
+        // $this->execute();
         
-        // User_post
-        $this->query("INSERT INTO user_post (user_id, post_id) VALUES
-            (1, 1),
-            (1, 2),
-            (1, 3),
-            (1, 4),
-            (1, 5),
-            (2, 6),
-            (2, 7),
-            (2, 8),
-            (2, 9),
-            (2, 10),
-            (3, 11),
-            (3, 12),
-            (3, 13),
-            (3, 14),
-            (3, 15),
-            (4, 16),
-            (4, 17),
-            (4, 18),
-            (4, 19),
-            (4, 20),
-            (5, 21),
-            (5, 22),
-            (5, 23),
-            (5, 24);"
-        );
-        $this->execute();
+        // // User_post
+        // $this->query("INSERT INTO user_post (user_id, post_id) VALUES
+        //     (1, 1),
+        //     (1, 2),
+        //     (1, 3),
+        //     (1, 4),
+        //     (1, 5),
+        //     (2, 6),
+        //     (2, 7),
+        //     (2, 8),
+        //     (2, 9),
+        //     (2, 10),
+        //     (3, 11),
+        //     (3, 12),
+        //     (3, 13),
+        //     (3, 14),
+        //     (3, 15),
+        //     (4, 16),
+        //     (4, 17),
+        //     (4, 18),
+        //     (4, 19),
+        //     (4, 20),
+        //     (5, 21),
+        //     (5, 22),
+        //     (5, 23),
+        //     (5, 24);"
+        // );
+        // $this->execute();
         
-        // Image 
-        $this->query("INSERT INTO images (post_id, img_path) VALUES
-            (1, 'http://localhost:8080/public/images/testing_images/1.jpeg'),
-            (1, 'http://localhost:8080/public/images/testing_images/2.jpeg'),
-            (1, 'http://localhost:8080/public/images/testing_images/3.jpeg'),
-            (1, 'http://localhost:8080/public/images/testing_images/4.jpeg'),
-            (1, 'http://localhost:8080/public/images/testing_images/5.jpeg'),
-            (2, 'http://localhost:8080/public/images/testing_images/2.jpeg'),
-            (3, 'http://localhost:8080/public/images/testing_images/3.jpeg'),
-            (4, 'http://localhost:8080/public/images/testing_images/4.jpeg'),
-            (5, 'http://localhost:8080/public/images/testing_images/5.jpeg'),
-            (6, 'http://localhost:8080/public/images/testing_images/6.jpeg'),
-            (7, 'http://localhost:8080/public/images/testing_images/7.jpeg'),
-            (8, 'http://localhost:8080/public/images/testing_images/8.jpeg'),
-            (9, 'http://localhost:8080/public/images/testing_images/9.jpeg'),
-            (10, 'http://localhost:8080/public/images/testing_images/10.jpeg'),
-            (11, 'http://localhost:8080/public/images/testing_images/11.jpeg'),
-            (12, 'http://localhost:8080/public/images/testing_images/12.jpg'),
-            (13, 'http://localhost:8080/public/images/testing_images/13.png'),
-            (14, 'http://localhost:8080/public/images/testing_images/14.jpg'),
-            (15, 'http://localhost:8080/public/images/testing_images/15.jpg'),
-            (16, 'http://localhost:8080/public/images/testing_images/16.jpg'),
-            (17, 'http://localhost:8080/public/images/testing_images/17.jpg'),
-            (18, 'http://localhost:8080/public/images/testing_images/18.jpg'),
-            (19, 'http://localhost:8080/public/images/testing_images/19.jpg'),
-            (20, 'http://localhost:8080/public/images/testing_images/20.jpg'),
-            (21, 'http://localhost:8080/public/images/testing_images/21.jpg'),
-            (22, 'http://localhost:8080/public/images/testing_images/22.gif');"
-        );
-        $this->execute();
+        // // Image 
+        // $this->query("INSERT INTO images (post_id, img_path) VALUES
+        //     (1, 'http://localhost:8080/public/images/testing_images/1.jpeg'),
+        //     (1, 'http://localhost:8080/public/images/testing_images/2.jpeg'),
+        //     (1, 'http://localhost:8080/public/images/testing_images/3.jpeg'),
+        //     (1, 'http://localhost:8080/public/images/testing_images/4.jpeg'),
+        //     (1, 'http://localhost:8080/public/images/testing_images/5.jpeg'),
+        //     (2, 'http://localhost:8080/public/images/testing_images/2.jpeg'),
+        //     (3, 'http://localhost:8080/public/images/testing_images/3.jpeg'),
+        //     (4, 'http://localhost:8080/public/images/testing_images/4.jpeg'),
+        //     (5, 'http://localhost:8080/public/images/testing_images/5.jpeg'),
+        //     (6, 'http://localhost:8080/public/images/testing_images/6.jpeg'),
+        //     (7, 'http://localhost:8080/public/images/testing_images/7.jpeg'),
+        //     (8, 'http://localhost:8080/public/images/testing_images/8.jpeg'),
+        //     (9, 'http://localhost:8080/public/images/testing_images/9.jpeg'),
+        //     (10, 'http://localhost:8080/public/images/testing_images/10.jpeg'),
+        //     (11, 'http://localhost:8080/public/images/testing_images/11.jpeg'),
+        //     (12, 'http://localhost:8080/public/images/testing_images/12.jpg'),
+        //     (13, 'http://localhost:8080/public/images/testing_images/13.png'),
+        //     (14, 'http://localhost:8080/public/images/testing_images/14.jpg'),
+        //     (15, 'http://localhost:8080/public/images/testing_images/15.jpg'),
+        //     (16, 'http://localhost:8080/public/images/testing_images/16.jpg'),
+        //     (17, 'http://localhost:8080/public/images/testing_images/17.jpg'),
+        //     (18, 'http://localhost:8080/public/images/testing_images/18.jpg'),
+        //     (19, 'http://localhost:8080/public/images/testing_images/19.jpg'),
+        //     (20, 'http://localhost:8080/public/images/testing_images/20.jpg'),
+        //     (21, 'http://localhost:8080/public/images/testing_images/21.jpg'),
+        //     (22, 'http://localhost:8080/public/images/testing_images/22.gif');"
+        // );
+        // $this->execute();
         
-        // Video 
-        $this->query("INSERT INTO videos (post_id, vid_path) VALUES
-            (1, 'http://localhost:8080/public/images/testing_images/xavier.mp4'),
-            (23, 'http://localhost:8080/public/images/testing_images/xavier.mp4'),
-            (24, 'http://localhost:8080/public/images/testing_images/xavier.mp4');"
-        );
-        $this->execute();
+        // // Video 
+        // $this->query("INSERT INTO videos (post_id, vid_path) VALUES
+        //     (1, 'http://localhost:8080/public/images/testing_images/xavier.mp4'),
+        //     (23, 'http://localhost:8080/public/images/testing_images/xavier.mp4'),
+        //     (24, 'http://localhost:8080/public/images/testing_images/xavier.mp4');"
+        // );
+        // $this->execute();
     }
     
     public function execute() {
