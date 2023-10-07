f5c53061299557dd565a36e658a8632a10a06ab7
louis <louiscaesapdf@gmail.com>
2023-10-07 22:42:22 +0700

    add error page

---
+++ app/controllers/Exceptionerror.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Exceptionerror extends Controller {
+    public function index($error_code = "404") {
+        try {
+            $data['error'] = $error_code;
+            $data['base'] = "http://localhost:8080/exceptionerror/";
+
+            $this->view('exceptionerror/index', $data);
+
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
+    
+}
